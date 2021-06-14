a9a843858a5e48c1448b76893af9756a39ace7ef
Lodhel <lodhelnew@gmail.com>
2021-06-14 17:28:33 +0300

    refactor

--- test_app/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from . import models
-
-
-class GraphicSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Graphic
-
-        fields = "__all__"
--- test_pr/settings.py
+++ test_pr/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'test_app',
-    'rest_framework'
 ]
 
 MIDDLEWARE = [
