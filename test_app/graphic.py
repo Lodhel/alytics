00960ce7dfa0bd1cdf0152113222572e1bec62c3
Lodhel <lodhelnew@gmail.com>
2021-06-14 14:20:29 +0300

    build graphic of formul

--- test_app/graphic.py
+++ test_app/graphic.py
@@ -1,77 +1,19 @@
 import datetime
-import string
-
 import matplotlib.pyplot as plt
+
+from string_parser import get_result, eval_
 
 
 class GraphicMixin:
 
-    @staticmethod
-    def _multiply(_x_array, _days):
-        return [_date_obj+datetime.timedelta(datetime.datetime.now().day*_days) for _date_obj in _x_array]
-
-    @staticmethod
-    def _split(_x_array, _days):
-        return [_date_obj-datetime.timedelta(datetime.datetime.now().day*_days) for _date_obj in _x_array]
-
-    @staticmethod
-    def _square(_x_array, _days):
-        return [_date_obj+datetime.timedelta(datetime.datetime.now().day**_days) for _date_obj in _x_array]
-
-    def _parse_string(self, string_input):
-        symbols_main = '^*/+-'
-        symbols_main_validated = ['^', '*', '/', '+', '-']
-        formula = [x for x in string_input if x in string.digits + symbols_main + 't']
-        validation = {
-            "t": None,
-            't2': None,
-            "operation": '',
-            "num": 0,
-            "sum": '',
-            "result": []
-        }
-
-        for symbol in formula:
-            if symbol == 't' and not validation['t']:
-                validation['sum'] += '{}'.format(symbol)
-                validation['t'] = True
-            if symbol == 't' and validation['t']:
-                validation['sum'] += '{}'.format(symbol)
-                validation['t2'] = True
-            if symbol in symbols_main_validated:
-                validation['sum'] += '{}'.format(symbol)
-                validation['operation'] = symbol
-            if symbol in string.digits:
-                validation['sum'] += '{}'.format(symbol)
-                validation['num'] = symbol
-
-            if validation['t'] and validation['operation'] and validation['num']:
-                if validation['operation'] == '*':
-                    validation['result'].append(self._multiply(
-                        [datetime.datetime.now() - datetime.timedelta(days=8), datetime.datetime.now()],
-                        int(validation['num'])
-                    ))
-
-            if validation['t'] and validation['operation'] and validation['t2']:
-                if validation['operation'] == '*':
-                    validation['result'].append(self._square(
-                        [datetime.datetime.now() - datetime.timedelta(days=8), datetime.datetime.now()],
-                        2
-                    ))
-
-            return validation['result']
-
     def create(self, x, y):
-        # x = [datetime.datetime.now() - datetime.timedelta(days=8), datetime.datetime.now()]
-        y_line = self._multiply(x, 5)  # TODO logic
+        x_line = [datetime.datetime.now() - datetime.timedelta(days=x), datetime.datetime.now()]
+        y_line = [get_result(y), datetime.datetime.now() - datetime.timedelta(days=int(eval_(y)))]
         plt.figure(1)
         plt.title('График функции |y| = {}'.format(y))
         plt.ylabel('Ось y')
         plt.xlabel('Ось x')
         plt.grid()
-        plt.plot(x, y_line)
+        plt.plot(x_line, y_line)
         plt.show()
 
-
-test = [GraphicMixin()._parse_string('t*5')]
-print(test)
---
+++ test_app/string_parser.py
@@ -0,0 +1,85 @@
+import datetime
+
+OPERATORS = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
+             '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y)}
+
+MONTHS = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+}
+
+
+def eval_(formula):
+    def parse(formula_string):
+        number = ''
+        for s in formula_string:
+            if s == 't':
+                number += str(datetime.datetime.now().day)
+            if s in '1234567890.':
+                number += s
+            elif number:
+                yield float(number)
+                number = ''
+            if s in OPERATORS or s in "()":
+                yield s
+        if number:
+            yield float(number)
+
+    def shunting_yard(parsed_formula):
+        stack = []
+        for token in parsed_formula:
+            if token in OPERATORS:
+                while stack and stack[-1] != "(" and OPERATORS[token][0] <= OPERATORS[stack[-1]][0]:
+                    yield stack.pop()
+                stack.append(token)
+            elif token == ")":
+                while stack:
+                    x = stack.pop()
+                    if x == "(":
+                        break
+                    yield x
+            elif token == "(":
+                stack.append(token)
+            else:
+                yield token
+        while stack:
+            yield stack.pop()
+
+    def calc(polish):
+        stack = []
+        for token in polish:
+            if token in OPERATORS:
+                y, x = stack.pop(), stack.pop()
+                stack.append(OPERATORS[token][1](x, y))
+            else:
+                stack.append(token)
+        return stack[0]
+
+    return calc(shunting_yard(parse(formula)))
+
+
+def get_result(formula):
+    day = int(eval_(formula))
+    month = int(datetime.datetime.now().month)
+    month_days = MONTHS[month]
+    year = int(datetime.datetime.now().year)
+
+    if day > month_days:
+        while day > month_days:
+            day -= month_days
+            month += 1
+            if month == 13:
+                month = 1
+                year += 1
+
+    return datetime.datetime(day=day, year=year, month=month)
